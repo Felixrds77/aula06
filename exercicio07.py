cf6a275d3874dc549623e271af738014002ffa77
Felixrds77 <felixcanarinho@gmail.com>
2025-04-09 11:15:40 -0300

    valor aritmético

---
+++ exercicio07.py
@@ -0,0 +1,6 @@
+print("digite 5 numeros para ver o valor aritmético deles")
+soma=0
+for z in range(1,6,1):
+    n = int(input("digite 1 numero: "))
+    soma += n
+print(f"o valor total é: {soma/5}")
